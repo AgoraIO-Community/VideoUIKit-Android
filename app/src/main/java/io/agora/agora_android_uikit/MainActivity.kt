1baa3957a5fd448a9bba7923415ecc54909c2e53
Max Cobb <maxxc@mac.com>
2021-04-30 16:25:10 +0100

    catch exception when bad app ID is used, check device permissions for camera and mic before joining channel

--- app/src/main/java/io/agora/agora_android_uikit/MainActivity.kt
+++ app/src/main/java/io/agora/agora_android_uikit/MainActivity.kt
@@ -3,8 +3,12 @@ package io.agora.agora_android_uikit
 import android.Manifest
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import io.agora.agorauikit_android.*
 import io.agora.rtc.Constants
 import io.agora.rtc.video.BeautyOptions
@@ -22,14 +26,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        agView = AgoraVideoViewer(
-            this, AgoraConnectionData("my-app-id"),
-            agoraSettings=this.settingsWithExtraButtons()
-        )
+        try {
+            agView = AgoraVideoViewer(
+                this, AgoraConnectionData("my-app-id"),
+                agoraSettings = this.settingsWithExtraButtons()
+            )
+        } catch (e: Exception) {
+            print("Could not initialise AgoraVideoViewer. Check your App ID is valid.")
+            print(e.message)
+            return
+        }
         val set = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
 
         this.addContentView(agView, set)
-        agView!!.join("test", role=Constants.CLIENT_ROLE_BROADCASTER)
+
+        // Check that the camera and mic permissions are accepted before attempting to join
+        if (AgoraVideoViewer.requestPermissions(this)) {
+            agView!!.join("test", role = Constants.CLIENT_ROLE_BROADCASTER)
+        } else {
+            val joinButton = Button(this)
+            joinButton.text = "Allow Camera and Microphone, then click here"
+            joinButton.setOnClickListener(View.OnClickListener {
+                // When the button is clicked, check permissions again and join channel
+                // if permissions are granted.
+                if (AgoraVideoViewer.requestPermissions(this)) {
+                    (joinButton.parent as ViewGroup).removeView(joinButton)
+                    agView!!.join("test", role=Constants.CLIENT_ROLE_BROADCASTER)
+                }
+            })
+            joinButton.setBackgroundColor(Color.GREEN)
+            joinButton.setTextColor(Color.RED)
+
+            this.addContentView(joinButton, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, 300))
+        }
     }
 
     fun settingsWithExtraButtons(): AgoraSettings {
