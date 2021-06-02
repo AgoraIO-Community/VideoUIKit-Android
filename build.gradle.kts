c07124d8ec2de5740c6ecfa4418368844a17f4b8
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2021-06-02 14:35:28 +0100

    using google maven (#14)
    Actually moved to the correct maven, now working with JitPack!

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,13 @@ buildscript {
         google()
 //        jcenter()
         mavenCentral()
+        maven { setUrl("https://maven.google.com") }
+        maven { setUrl("https://maven.fabric.io/public") }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:4.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+//        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
@@ -16,10 +19,13 @@ buildscript {
 
 allprojects {
     repositories {
+        mavenCentral()
         google()
 //        jcenter()
         mavenCentral()
         maven { setUrl("https://jitpack.io") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
     }
 }
 
